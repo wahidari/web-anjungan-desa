5f07acda881a1936467241a37defed1c06570c6e
wahidari <wahiid.ari@gmail.com>
2022-01-21 22:03:43 +0700

    fix responsive issue in wide screen

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer({namaDesa}) {
     return (
         <>
-            <footer className="pt-3 pb-1">
+            <footer className="pt-3 pb-1 mt-auto">
                 <div className="container">
                     <p className="text-center text-secondary">© Hak Cipta Dilindungi 2021 - Sistem Resmi Desa <a href="https://web-profile-desa.vercel.app/"
                         className="text-decoration-none" target="_blank" rel="noreferrer">{namaDesa}</a>. All rights reserved</p>
